97263dbbf699c5fcaea76b5552b3e6708f563eb9
Eduard <edik_schneider@hotmail.com>
2019-06-11 12:54:56 +0200

    Added clickable points

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
 const COUNTRIES =
-  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_scale_rank.geojson'; //eslint-disable-line
-const AIR_PORTS =
-  'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
+  'countries.geojson';
+const COUNTRY_POINTS =
+  'countrypoints.geojson';
 
 const INITIAL_VIEW_STATE = {
-  latitude: 51.47,
-  longitude: 0.45,
+  latitude: 46.227638,
+  longitude: 2.213749,
   zoom: 4,
   bearing: 0,
-  pitch: 30
+  pitch: 20
 };
 
 const deck = new Deck({
@@ -17,44 +16,32 @@ const deck = new Deck({
   controller: true,
   layers: [
     new GeoJsonLayer({
-      id: 'base-map',
+      id: 'base_map',
       data: COUNTRIES,
       // Styles
-      stroked: true,
       filled: true,
       lineWidthMinPixels: 2,
       opacity: 0.4,
-      getLineDashArray: [3, 3],
       getLineColor: [60, 60, 60],
-      getFillColor: [200, 200, 200]
+      getFillColor: [200, 200, 255],
+    //   pickable: true,
+    //   autoHighlight: true,
+    //   onClick: info =>
+    //     info.object && alert(`${info.object.properties.name} (${info.object.id})`)
     }),
+
     new GeoJsonLayer({
-      id: 'airports',
-      data: AIR_PORTS,
-      // Styles
+      id: 'country_points',
+      data: COUNTRY_POINTS,
       filled: true,
-      pointRadiusMinPixels: 2,
+      lineWidthMinPixels: 20,
       opacity: 1,
-      pointRadiusScale: 2000,
-      getRadius: f => 11 - f.properties.scalerank,
-      getFillColor: [200, 0, 80, 180],
-      // Interactive props
+      getLineColor: [200, 255, 200],
+      getFillColor: [200, 255, 200],
       pickable: true,
       autoHighlight: true,
       onClick: info =>
-        // eslint-disable-next-line
-        info.object && alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
-    }),
-    new ArcLayer({
-      id: 'arcs',
-      data: AIR_PORTS,
-      dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
-      // Styles
-      getSourcePosition: f => [-0.4531566, 51.4709959], // London
-      getTargetPosition: f => f.geometry.coordinates,
-      getSourceColor: [0, 128, 200],
-      getTargetColor: [200, 0, 80],
-      getWidth: 1
+        info.object && alert(`${info.object.properties.name} (${info.object.id})`)
     })
   ]
 });
